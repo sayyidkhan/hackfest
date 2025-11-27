80988a2d0b93aa0e7a295e8c6411a108b04dbe86
sayyidkhan <sayyidkhan92@hotmail.com>
2025-11-27 08:44:52 +0800

    refactor to use netlify:

--- frontend/react-router.config.ts
+++ frontend/react-router.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+  future: {
+    // Enable compatibility with the Cloudflare Vite plugin
+    unstable_viteEnvironmentApi: true,
+  },
 } satisfies Config;
